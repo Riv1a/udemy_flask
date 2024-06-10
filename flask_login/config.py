6b43392e1cb2d20f2860a828da5222f09a08774c
H3bi <165383340+h3bi-0@users.noreply.github.com>
2024-06-10 15:01:45 +0200

    Update config.py

--- flask_login/config.py
+++ flask_login/config.py
@@ -5,7 +5,7 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
-app.config['SECRET_KEY'] = 'kitsune'
+app.config['SECRET_KEY'] = ''
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
