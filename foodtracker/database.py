3ed59d4d7da015ae7bb7dcae2a552923337683b6
H3bi <165383340+h3bi-0@users.noreply.github.com>
2024-06-10 15:14:54 +0200

    Update database.py

--- foodtracker/database.py
+++ foodtracker/database.py
@@ -2,7 +2,7 @@ import sqlite3
 from flask import g
 
 def connect_db():
-    sql = sqlite3.connect('/Users/florianschmitt/desktop/udemyflask/foodtracker/website/database/food_log.db')
+    sql = sqlite3.connect('') # Insert Database Path
     sql.row_factory = sqlite3.Row
     return sql
 
